05333e63f03e3cd2029a1cd198ff3c549f1cb868
tatyana <tilkka@mail.ru>
2024-01-17 02:19:55 +0300

    feat: finish third part of task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const { readdir, stat } = require('fs/promises');
+const pathToDir = path.join(__dirname, "secret-folder");
+
+async function getFilesfromDir(pathToDir) {
+  const dirContent = await readdir(pathToDir);
+
+  dirContent.forEach(async (content) => {
+    const pathToFile = path.join(pathToDir, content);
+    const fileInfo = await stat(pathToFile);
+
+    if (!fileInfo.isDirectory()) {
+      displayFileData(fileInfo.size, pathToFile);
+    }
+  })
+}
+const displayFileData = (fileSize, pathToFile) => {
+  const sizeInKb = fileSize / 1024;
+  const extention = path.extname(pathToFile).substring(1);
+  const basename = path.basename(pathToFile, `.${extention}`);
+  console.log(`${basename} - ${extention} - ${sizeInKb.toFixed(3)}Kb`);
+}
+getFilesfromDir(pathToDir);
